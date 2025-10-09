51ecbe8b15e2e7f8969fedb6450680d076350565
Alex Fernández <AlexChagi@gmail.com>
2025-10-09 07:51:54 +0000

    Solo se ilumina como 1px, no se nota nada

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -32,7 +32,7 @@ const ResizableHandle = ({
 }) => (
   <PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-px after:bg-border after:content-[''] data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-px data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-y-1/2",
+      "relative flex w-2 items-center justify-center bg-transparent after:absolute after:h-full after:w-px after:bg-border after:content-[''] data-[panel-group-direction=vertical]:h-2 data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:h-px data-[panel-group-direction=vertical]:after:w-full",
       "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1",
       "transition-colors hover:bg-primary/20",
       className
