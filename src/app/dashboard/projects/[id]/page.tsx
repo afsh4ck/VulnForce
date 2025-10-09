ec80d188d998dad1f23a016100be842c7f69e5a5
Alex Fernández <AlexChagi@gmail.com>
2025-10-09 08:29:59 +0000

    Ha vuelto el error de la doble línea en el editor, corrígelo

--- src/app/dashboard/projects/[id]/page.tsx
+++ src/app/dashboard/projects/[id]/page.tsx
@@ -369,13 +369,12 @@ const ScopeSectionEditor = ({ section, onContentChange, onDelete, view, onViewCh
                                         value={section.content}
                                         onValueChange={(newContent) => onContentChange(newContent)}
                                         onPaste={handlePaste}
-                                        className="h-full"
                                     />
                                     </div>
                                 </ResizablePanel>
                                 <ResizableHandle />
                                 <ResizablePanel defaultSize={50}>
-                                <div className="h-full overflow-auto rounded-md p-4 border-l">
+                                <div className="h-full overflow-auto rounded-md p-4">
                                     <MarkdownPreview content={section.content} getImage={getImage} />
                                 </div>
                                 </ResizablePanel>
@@ -383,7 +382,7 @@ const ScopeSectionEditor = ({ section, onContentChange, onDelete, view, onViewCh
                         </div>
                     )}
                     {view === 'edit' && (
-                        <div className="h-full">
+                        <div>
                             <HighlightingTextarea
                                 ref={textareaRef}
                                 value={section.content}
@@ -1016,6 +1015,7 @@ export default function ProjectDetailsPage() {
     
 
     
+
 
 
 
