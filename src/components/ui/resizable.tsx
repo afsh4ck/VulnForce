d8deb674ff58b69270e45f2f43cc4c69066bb26d
Alex Fernández <AlexChagi@gmail.com>
2025-10-09 07:50:20 +0000

    Ahora funciona bien pero no se ilumina

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -32,9 +32,9 @@ const ResizableHandle = ({
 }) => (
   <PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border",
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-px after:bg-border after:content-[''] data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-px data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-y-1/2",
       "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1",
-      "data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full",
+      "transition-colors hover:bg-primary/20",
       className
     )}
     {...props}
