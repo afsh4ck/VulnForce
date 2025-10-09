f3cd6ed03e62f59ff0a60acb614c004587598df6
Alex Fernández <AlexChagi@gmail.com>
2025-10-09 09:22:54 +0000

    La barra divisoria se ilumina distinto a la parte de edición del informe

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -33,12 +33,8 @@ const ResizableHandle = ({
 }) => (
   <PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border",
-      "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1",
-      "data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full",
-      "[&[data-panel-group-direction=horizontal]]:hover:w-2 [&[data-panel-group-direction=vertical]]:hover:h-2",
-      "[&[data-panel-group-direction=horizontal]]:active:w-2 [&[data-panel-group-direction=vertical]]:active:h-2",
-      "transition-all",
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:hover:bg-primary [&[data-panel-group-direction=vertical]]:hover:bg-primary",
+      "data-[resize-handle-state=drag]:bg-primary",
       className
     )}
     {...props}
