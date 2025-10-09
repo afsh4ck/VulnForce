6b9d34acc74eabaeb3b1c851f071123c130dacce
Alex Fernández <AlexChagi@gmail.com>
2025-10-09 09:24:23 +0000

    Se ilumina distinto, no es una línea amarilla potente, sino como una luz

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -33,8 +33,10 @@ const ResizableHandle = ({
 }) => (
   <PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:hover:bg-primary [&[data-panel-group-direction=vertical]]:hover:bg-primary",
-      "data-[resize-handle-state=drag]:bg-primary",
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:translate-x-0",
+      "transition-shadow duration-200",
+      "[&[data-panel-group-direction=horizontal]]:hover:shadow-[0_0_2px_1px_hsl(var(--primary))]",
+      "[&[data-panel-group-direction=horizontal]][data-resize-handle-state=drag]:shadow-[0_0_2px_1px_hsl(var(--primary))]",
       className
     )}
     {...props}
