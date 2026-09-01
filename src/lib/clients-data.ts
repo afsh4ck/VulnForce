d66dd909f1929a5659a9f866edb8ada564b94980
afsh4ck <afsh4ck@proton.me>
2026-09-01 10:30:07 +0200

    Anadir logos PNG embebidos de los clientes

--- src/lib/clients-data.ts
+++ src/lib/clients-data.ts
@@ -1,8 +1,9 @@
 import type { Client } from './types';
+import { CLIENT_LOGOS } from './client-logos';
 
 export const initialClients: Client[] = [
-  { id: 'cli-htb', name: 'Hack The Box', contact: 'contact@hackthebox.eu', phone: '+1-202-555-0182', logoUrl: '' },
-  { id: 'cli-ine', name: 'INE Security', contact: 'security@ine.com', phone: '+1-202-555-0182', logoUrl: '' },
-  { id: 'cli-offsec', name: 'Offsec', contact: 'audit@offsec.com', phone: '+1-202-555-0182', logoUrl: '' },
-  { id: 'cli-h4ck', name: 'h4ckercademy', contact: 'info@h4ckercademy.com', phone: '+1-202-555-0182', logoUrl: '' },
+  { id: 'cli-htb', name: 'Hack The Box', contact: 'contact@hackthebox.eu', phone: '+1-202-555-0182', logoUrl: CLIENT_LOGOS['cli-htb'] },
+  { id: 'cli-ine', name: 'INE Security', contact: 'security@ine.com', phone: '+1-202-555-0182', logoUrl: CLIENT_LOGOS['cli-ine'] },
+  { id: 'cli-offsec', name: 'Offsec', contact: 'audit@offsec.com', phone: '+1-202-555-0182', logoUrl: CLIENT_LOGOS['cli-offsec'] },
+  { id: 'cli-h4ck', name: 'h4ckercademy', contact: 'info@h4ckercademy.com', phone: '+1-202-555-0182', logoUrl: CLIENT_LOGOS['cli-h4ck'] },
 ];
